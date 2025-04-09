cce22a2b2551b980d7d2a1ea8538fb4d04169068
Kenan Ege <61656752+KenanAegean@users.noreply.github.com>
2025-04-10 01:24:13 +0300

    new changes

--- Source/SnakeGame/SnakeWorld.cpp
+++ Source/SnakeGame/SnakeWorld.cpp
@@ -2,6 +2,7 @@
 
 
 #include "SnakeWorld.h"
+#include "Engine/World.h"
 
 // Sets default values
 ASnakeWorld::ASnakeWorld()
@@ -14,6 +15,14 @@ ASnakeWorld::ASnakeWorld()
 	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
 
 	WallMeshInstances->SetupAttachment(RootComponent);
+
+	InstancedWalls = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstancedWalls"));
+
+	InstancedWalls->SetupAttachment(RootComponent);
+
+	InstancedFloors = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("InstancedFloors"));
+
+	InstancedFloors->SetupAttachment(RootComponent);
 
 	// Here you can get started on creating your level
 	/*
@@ -35,6 +44,68 @@ ASnakeWorld::ASnakeWorld()
 	for (int x = 0; x < 5000; x += 200)
 	{
 		WallMeshInstances->AddInstance(FTransform(FRotator::ZeroRotator, FVector(x, 200.0f, 0.0f)));
+	}
+}
+
+void ASnakeWorld::OnConstruction(const FTransform& Transform)
+{
+	UE_LOG(LogTemp, Log, TEXT("OnConstruction Called!"));
+
+	// Clean up
+	InstancedFloors->ClearInstances();
+
+	InstancedWalls->ClearInstances();
+
+	for (auto& Actor : SpawnedActors)
+	{
+		Actor->Destroy();
+	}
+
+	SpawnedActors.Empty();
+
+	// Load level
+	TArray<FString> Lines;
+	FString FilePath = FPaths::ProjectDir() + TEXT("Levels/Level1.txt");
+
+	if (FFileHelper::LoadFileToStringArray(Lines, *FilePath))
+	{
+		int y = 0;
+		for (const FString& Line : Lines)
+		{
+			for (int x = 0; x < Line.Len(); x++)
+			{
+				// Need to invert Y and switch X and Y to get the map loaded with X as a forward vector (instead of -Y that is in the text file)
+				FTransform Offset = FTransform(FRotator::ZeroRotator, FVector((Lines.Num() - y) * 100, x * 100, 0.0f));
+				switch (Line[x])
+				{ 
+				case '#':
+					InstancedWalls->AddInstance(Offset);
+					break;
+				case 'O':
+					// Trapdoor
+						break;
+				case 'D':
+					InstancedFloors->AddInstance(Offset);
+					// Door
+					if (IsValid(DoorActor))
+					{
+						AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(DoorActor, Offset, FActorSpawnParameters());
+						if (IsValid(SpawnedActor))
+						{
+							SpawnedActor->AttachToActor(this, FAttachmentTransformRules::KeepRelativeTransform);
+
+							SpawnedActors.Add(SpawnedActor);
+						}
+					}
+					break;
+				case '.':
+					InstancedFloors->AddInstance(Offset);
+					break;
+				}
+			}
+
+			y++;
+		}
 	}
 }
 
--- Source/SnakeGame/SnakeWorld.h
+++ Source/SnakeGame/SnakeWorld.h
@@ -19,6 +19,23 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UInstancedStaticMeshComponent* WallMeshInstances;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	USceneComponent* SceneComponent;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UInstancedStaticMeshComponent* InstancedWalls;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UInstancedStaticMeshComponent* InstancedFloors;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TSubclassOf<AActor> DoorActor;
+
+	UPROPERTY()
+	TArray<AActor*> SpawnedActors;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
