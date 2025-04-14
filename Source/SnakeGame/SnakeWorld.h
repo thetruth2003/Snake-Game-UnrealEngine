4f01327c770e45e1074424ca9b6c6398983157ea
Kenan Ege <61656752+KenanAegean@users.noreply.github.com>
2025-04-14 22:17:47 +0300

    spawn logic codes

--- Source/SnakeGame/SnakeWorld.h
+++ Source/SnakeGame/SnakeWorld.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// SnakeWorld.h
 
 #pragma once
 
@@ -11,51 +11,55 @@ UCLASS()
 class SNAKEGAME_API ASnakeWorld : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+    
+public:    
 	// Sets default values for this actor's properties
 	ASnakeWorld();
 
-	//UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	//UInstancedStaticMeshComponent* WallMeshInstances;
-
+	// Root scene component.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	USceneComponent* SceneComponent;
 
+	// Instanced mesh component for walls.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	UInstancedStaticMeshComponent* InstancedWalls;
 
+	// Instanced mesh component for floors.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	UInstancedStaticMeshComponent* InstancedFloors;
 
+	// Optional door actor class.
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	TSubclassOf<AActor> DoorActor;
 
-	// Food actor class to spawn.
+	// Food actor class to spawn (the apple).
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Food")
 	TSubclassOf<AActor> FoodClass;
 
-	// Spawn interval (in seconds) for food.
+	// (Optional) Delay before spawning the next apple.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Food")
-	float FoodSpawnInterval = 5.0f;
+	float FoodSpawnDelay = 0.5f;
 
+	// Actors spawned as part of level construction (e.g., doors).
 	UPROPERTY()
 	TArray<AActor*> SpawnedActors;
 
+	// Override OnConstruction to read the level layout and build the world.
 	virtual void OnConstruction(const FTransform& Transform) override;
-
-protected:
-	FTimerHandle FoodTimerHandle;
-
-	// Function to spawn food.
+    
+	// Spawns a food actor on a random floor tile.
 	UFUNCTION()
 	void SpawnFood();
-	
-	// Called when the game starts or when spawned
+
+protected:
+	// Called when the game starts or when spawned.
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
+public:    
+	// Called every frame.
 	virtual void Tick(float DeltaTime) override;
 
+private:
+	// Array holding locations of floor tiles (read from the level file).
+	TArray<FVector> FloorTileLocations;
 };
